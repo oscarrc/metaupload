cebbbba3e937c88723c5274a1efcdb699162e895
Oscar <oscarrc.web@gmail.com>
2021-12-06 15:03:21 +0100

    [MOD] Powered by link

--- src/components/sections/PoweredBy.js
+++ src/components/sections/PoweredBy.js
@@ -3,7 +3,9 @@ const PoweredBy = () => {
         <section id="poweredbyIPFS">
             <div className="container">                
                 <h2>Powered by</h2>
-                <img src="/img/ipfs.svg" alt="IPFS" />
+                <a href="https://ipfs.io/" target="_BLANK" rel="noreferrer">
+                    <img src="/img/ipfs.svg" alt="IPFS" />
+                </a>
             </div>
         </section>
     )
