90aed1a2f18dc48f7702e0166e22b6a0890ce403
Oscar <oscarrc.web@gmail.com>
2021-12-14 11:17:21 +0100

    [MOD] Target blank external links

--- src/components/sections/CallToAction.js
+++ src/components/sections/CallToAction.js
@@ -15,11 +15,11 @@ const CallToAction = () => {
                         </button> :
                         null
                     }
-                    <a role="button" className="contrast" href="https://ko-fi.com/oscarrc">
+                    <a role="button" target="_BLANK" rel="noreferrer" className="contrast" href="https://ko-fi.com/oscarrc">
                         <img src="/img/kofi.png" alt="Support me on Ko-fi"/>
                         Support me on Ko-fi
                     </a>
-                    <a role="button" className="secondary" href="https://github.com/oscarrc/metaupload">
+                    <a role="button" target="_BLANK" rel="noreferrer" className="secondary" href="https://github.com/oscarrc/metaupload">
                         <img src="/img/github.png" alt="Contribute on Github"/>
                         Contribute on Github
                     </a>
