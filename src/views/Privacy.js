3fb64b5c8f9f426cd985cbf5761622321620ee65
Oscar <oscarrc.web@gmail.com>
2021-12-14 12:14:03 +0100

    [ADD] Privacy and terms

--- src/views/Privacy.js
+++ src/views/Privacy.js
@@ -13,6 +13,11 @@ const Privacy = () => {
             <section id="policy">
                 <div className="container">   
                     <h2>Privacy policy</h2>
+                    <p className="prose">METAUPLOAD&trade; (from now on "the software") does not store any user personal data, nor will collect and / or share any information with third parties</p>
+                    <p className="prose">All the information is stored on the user's computer and, due to the decentralized nature of the software, it's also stored on other users' computers.
+                       Those users are not aware of the data stored on their computers since it's encrypted. They're merely seeders for your content.
+                    </p>
+                    <p className="prose"><strong>Although all the information is encrypted, the software hasn't been audited, so use it at your own risk.</strong></p>
                 </div>
             </section>
         </div>
